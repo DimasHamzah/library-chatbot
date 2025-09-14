7de17fcf7d749b7a05604148123e2b97840b93fd
dimas nurhamzah <hamzahdimas85@gmail.com>
2025-09-14 22:32:53 +0700

    add custom response validator

--- routes/role.js
+++ routes/role.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const roleController = require('../app/controllers/RoleController');
+const { validateRole } = require('../app/middlewares/roleValidator');
 
-router.post('/', roleController.create);
+router.post('/', validateRole, roleController.create);
 router.get('/', roleController.findAll);
 router.get('/:id', roleController.findById);
-router.put('/:id', roleController.update);
+router.put('/:id', validateRole, roleController.update);
 router.delete('/:id', roleController.delete);
 
 module.exports = router;
